b27a61440e0eb70bb1a9bac50b307d72ac9f701e
Don't feel bad <116130475+openpolicy0@users.noreply.github.com>
2023-04-22 15:18:27 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -57,15 +57,40 @@ with open(csv_f, 'r') as f:
         file.append(row[0])
 
 def extract(proxy):
-    #this was for when we took a list into the function, without conc futures.
-    #proxy = random.choice(proxylist)
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0'}
     try:
-        #change the url to https://httpbin.org/ip that doesnt block anything
-        r = requests.get('https://httpbin.org/ip', headers=headers, proxies={'http' : proxy,'https': proxy}, timeout=2)
+        r = requests.get('https://httpbin.org/ip', headers=headers, proxies={'socks4' : proxy,'https': proxy}, timeout=2)
         print('     200         |            UP ✅️      |             '+r.json()['origin'])
     except:
         print('     404         |           DOWN ❌     |             faild')
+    return proxy
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(extract, file)
+
+print("[light_green]=(finding other proxys in your list)================================================[/light_green]")
+sleep(1)
+print("[light_green]=(simplifying proxys)===============================================================[/light_green]")
+sleep(1)
+print("[light_green]getting proxy IPs...[/light_green]")
+sleep(1)
+print("[light_green]getting region...[/light_green]")
+sleep(2)
+print("[light_green]getting country...[/light_green]")
+sleep(2)
+print("[light_green]===========================================================================[/light_green]")
+with open(csv_f, 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        file.append(row[0])
+
+def extract(proxy):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0'}
+    try:
+        r = requests.get('https://ipinfo.io/json', headers=headers, proxies={'http' : proxy,'https': proxy}, timeout=2)
+        print('     200         |       UP ✅️      |     '+r.json()['ip'], r.json()['region'], r.json()['country'])
+    except:
+        pass
     return proxy
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
